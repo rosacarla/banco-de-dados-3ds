954396e040e83bda35b500f7c9cb14a1cab3a1dd
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-09-02 12:28:11 -0300

    Update app-estoque.py

--- app-estoque.py
+++ app-estoque.py
@@ -38,6 +38,11 @@ produtos = [
     Produto(3, "Borracha", "Papelaria", 200, 1.20, "Mercur")
 ]
 
+# ➕ Adiciona novos produtos à lista existente
+produtos.append(Produto(4, "Marcador", "Papelaria", 80, 4.75, "Pilot"))
+produtos.append(Produto(5, "Grampeador", "Escritório", 30, 22.90, "Tramontina"))
+
+
 # 🧬 Serializa os produtos para JSON e exibe no terminal
 json_produtos = json.dumps([p.to_dict() for p in produtos], indent=4)
 print("📦 Produtos em JSON:")
